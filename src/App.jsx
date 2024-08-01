28ebce711eda1b8f583904386ce79d118009b567
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-01 15:43:09 -0700

    made pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import SignIn from "./pages/SignIn"
+import Home from "./pages/Home"
+import SignOut from "./pages/SignOut"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
 
 export default function () {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
